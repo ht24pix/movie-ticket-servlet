fe7dac7d2e7b25aa7d830f5e36be9ac990eab15f
ht24pix <huutri1912@gmail.com>
2026-03-16 22:58:31 +0700

    up-to-date 01

---
+++ movie-ticket-purchase/src/main/java/com/ht24/movieticket/controller/MovieTicketController.java
@@ -0,0 +1,24 @@
+package com.ht24.movieticket.controller;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/movies")
+public class MovieTicketController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8839877152283498192L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
+}
